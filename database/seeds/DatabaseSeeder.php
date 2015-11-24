562b749990f9d47038534a8d35bc1b73371f1c75
Paolo Davila <paolodavila@iesebre.com>
2015-11-24 21:16:30 +0100

    Add models and migrates tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Tag;
+use App\Task;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,8 +16,35 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $faker = Faker\Factory::create();
+        $this->sendTasks($faker);
+        $this->sendTags($faker);
+
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
+    }
+
+    private function sendTasks($faker)
+    {
+        foreach (range(0,100) as $number) {
+            $task = new Task();
+
+            $task->name = $faker->sentence;
+            $task->done = $faker->boolean;
+            $task->priority = $faker->randomDigit;
+            $task->save();
+        }
+    }
+
+    private function sendTags($faker)
+    {
+        foreach (range(0,100) as $number) {
+            $tag = new Tag();
+
+            $tag->name = $faker->word;
+            $tag->save();
+        }
+
     }
 }
